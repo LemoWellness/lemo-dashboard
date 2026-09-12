afa8a81b3268d67c85e119c2e352bdbaffb28f8a
Gloria <gloria@lemowellness.com>
2026-09-12 10:29:23 -0700

    Add app icon and web manifest

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Lora:ital@0;1&family=Poppins:wght@300;500&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#1C1916" />
       </Head>
       <body>
         <Main />
